0914aee5506233c4454884e91adf38bd157796fa
unknown <charanpoola13@gmail.com>
2024-02-16 13:04:48 +0530

    mental

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -2,11 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:movie_app_assignment/api/api.dart';
 import 'package:movie_app_assignment/favourites.dart';
 import 'package:movie_app_assignment/models/movie.dart';
+// ignore: depend_on_referenced_packages
 import 'package:google_fonts/google_fonts.dart';
 import 'package:movie_app_assignment/search_page.dart';
 import 'package:movie_app_assignment/trending_slider.dart';
-
-
+import 'package:movie_app_assignment/login_page.dart';
+import 'package:movie_app_assignment/signup_page.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -27,6 +29,8 @@ late Future<List<Movie>> popularMovies;
   
   }
 
+  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,10 +43,10 @@ late Future<List<Movie>> popularMovies;
               ),
               ],
               ),
-        backgroundColor: Color.fromARGB(255, 21, 119, 135), // colour of top app bar
+        backgroundColor: const Color.fromARGB(255, 21, 119, 135), // colour of top app bar
       ),
 
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),//represents home page background colour
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),//represents home page background colour
       body : SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         child: Column(
@@ -52,7 +56,7 @@ late Future<List<Movie>> popularMovies;
               'Popular Movies',
               style: GoogleFonts.aBeeZee(fontSize:18),
               ),
-              SizedBox(height: 20,), 
+              const SizedBox(height: 20,), 
               SizedBox(
                 child: FutureBuilder(
                   future: popularMovies, 
@@ -74,6 +78,27 @@ late Future<List<Movie>> popularMovies;
                     }
                   }),
               ) ,
+
+
+              const SizedBox(height: 20,),
+
+              Center( child :Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {
+                  final Uri url_link = Uri.parse('https://www.themoviedb.org/movie');
+                  launchUrl(url_link);
+                },
+                child: const Text(
+                  'Click here for more',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            ),
             
           ],
         )
@@ -103,6 +128,20 @@ late Future<List<Movie>> popularMovies;
               title: const Text('Favourite'),
               onTap: () {
                 Navigator.push(context, MaterialPageRoute(builder: (context) => const Favorites()));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.login),
+              title: const Text('Login'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage()));
+              },
+            ),
+             ListTile(
+              leading: const Icon(Icons.create),
+              title: const Text('SignUp'),
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const SignupPage()));
               },
             ),
           ],
---
+++ lib/signup_page.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app_assignment/home_page.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text("Signup Page"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>
+          [
+            const Padding(
+              padding: EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: SizedBox(
+                    width: 200,
+                    height: 100,),
+              ),
+            ),
+
+            const Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Email',
+                    hintText: 'Enter valid email id as abc@gmail.com'),
+              ),
+            ),
+
+            const Padding(
+              padding: EdgeInsets.only(
+              left: 15.0, right: 15.0, top: 15, bottom: 0),
+              child: TextField(
+
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+
+            const Padding(
+              padding: EdgeInsets.only(
+              left: 15.0, right: 15.0, top: 15, bottom: 0),
+              child: TextField(
+
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Confirm Password',
+                    hintText: 'Keep in mind'),
+              ),
+            ),
+
+            TextButton(
+              onPressed: () {
+          
+              },
+              child: const Text(
+                'Forgot Password',
+                style: TextStyle(color: Colors.blue, fontSize: 15),
+              ),
+            ),
+
+            Container(
+              height: 50,
+              width: 250,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              child: TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=> const HomePage()));
+                },
+                child: const Text(
+                  'Signup',
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 130,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
